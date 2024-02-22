fef748cb434c9e1819be5b816460043f31542765
ErminiaDev <jeanningros.marine@gmail.com>
2024-02-22 12:41:23 +0100

    change CSS

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,34 +1,47 @@
 import { useState } from "react";
 
-export default function Player({symbol, initialName, isActive, onChangeName }) {
-	const [playerName, setPlayerName] = useState(initialName);
+export default function Player({
+  symbol,
+  initialName,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState();
 
   function handleEditClick() {
     //setIsEditing(!isEditing) because of state update scheduling
     setIsEditing((editing) => !editing);
-    if(isEditing){
+    if (isEditing) {
       onChangeName(symbol, playerName);
     }
   }
 
-  function handleChange(event){
-	setPlayerName(event.target.value)
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
-    <li className={isActive? "active": undefined}>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+      <button onClick={handleEditClick}>
+        {isEditing ? (
+          <i class="fa-regular fa-floppy-disk"></i>
+        ) : (
+          <i class="fa-regular fa-pen-to-square"></i>
+        )}
+      </button>
     </li>
   );
 }
